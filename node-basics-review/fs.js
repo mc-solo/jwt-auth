0ef9bfccf2256598ca601e1a9fe013154866ef99
Wondwosen <revatoel@gmail.com>
2024-09-06 02:51:04 +0300

    working with fs.writeFile and fs.readFile methods

--- node-basics-review/fs.js
+++ node-basics-review/fs.js
@@ -1,6 +1,8 @@
 import fs from "fs";
 
 // reading data
+// try catch is designed to handle errors for synchronous codes
+// for asynchronous codes like this errors should be handled like this by using callback functions
 
 fs.readFile("test.txt", "utf-8", (err, data) => {
   if (err) {
@@ -9,4 +11,16 @@ fs.readFile("test.txt", "utf-8", (err, data) => {
   } else {
     console.log(data);
   }
+});
+
+// writing a file
+const myData =
+  "This is supposed to be written after the execution of the below function";
+
+fs.writeFile("writtenFile.txt", myData, (err) => {
+  if (err) {
+    throw new Error("Error writing file", err);
+    return;
+  }
+  console.log("File has been saved!");
 });
