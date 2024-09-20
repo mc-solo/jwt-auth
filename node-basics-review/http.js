2bc9281f6239143dfa71e6f53e006e84e0acb713
Wondwosen <revatoel@gmail.com>
2024-09-20 23:19:06 +0300

    simulating fetch data with the GET http method

--- node-basics-review/http.js
+++ node-basics-review/http.js
@@ -1,7 +1,34 @@
 import http from "http";
+import express from "express";
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // a status code for okay
-  res.setHeader("Content-Type", "text/plain"); // set response header
-  res.end("Hello, world!"); //send response body and end the response
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200; // a status code for okay
+//   res.setHeader("Content-Type", "text/plain"); // set response header
+//   res.end("Hello, world!"); //send response body and end the response
+// });
+
+// the different http methods
+const app = express();
+const port = 3001;
+
+const application = app.get("/users", (req, res) => {
+  // stimulating fetching data
+  const users = [
+    {
+      id: 1,
+      name: "John Doe",
+      age: 47,
+    },
+    {
+      id: 2,
+      name: "Helina",
+      age: 20,
+    },
+  ];
+
+  res.json(users);
+});
+
+application.listen(port, () => {
+  console.log("application is already running ");
 });
